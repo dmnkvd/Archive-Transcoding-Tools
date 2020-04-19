6811e24fe4634cbd10cca9db3c66f05d8c27b278
dmnkvd <dominikvr@gmail.com>
2020-04-19 14:12:25 +0200

    tool 2

---
+++ tool_2-gradual-disclosure/test.js
@@ -0,0 +1,32 @@
+badge = {
+    'A',
+    'pink': {
+        #
+    },
+    'badge': {
+        'metallic';
+        'circular object': {
+            'circle'
+        }
+    },
+    'with'
+    'black English text': {
+        'English': {
+            'black': {
+                #000000
+            },
+            'text': {
+                'line': {
+                    'word': "PEACE"
+                },
+                'line': {
+                    'word': "IN",
+                    'word': "THE"
+                },
+                'line': {
+                    'word': "BALKANS"
+                }
+            }
+        };
+    }
+}
