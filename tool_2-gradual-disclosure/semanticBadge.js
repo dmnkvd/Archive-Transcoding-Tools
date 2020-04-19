172d16aa4a7665c856bf1b68802a38eefa593671
dmnkvd <dominikvr@gmail.com>
2020-04-19 17:16:54 +0200

    text to array

---
+++ tool_2-gradual-disclosure/semanticBadge.js
@@ -0,0 +1,158 @@
+// Semantic Deconstruction of a photograph of a Badge
+// What do I see?
+// What does it mean?
+
+badgeSemantics = {
+    'A',
+    'pink': {
+        'denotation': {
+            '#DA9BB0'
+        }
+        'connotation': {
+            'gentle',
+            'flesh',
+            'skin',
+            'personal'
+        }
+    },
+    'metallic badge': {
+        'metallic'; {
+            'denotation': {
+                'made of metal'
+            }
+            'connotation': {
+                'sturdy'
+                'permanent'
+            }
+        }
+        'badge' {
+            'denotation': {
+                'circular object': {},
+                'accessory': {}
+            }
+
+            'connotation': {
+                'identity',
+                'identification',
+                'social cause',
+                'subculture',
+                'belonging'
+            }
+        }
+    },
+    'with'
+    'capitalised black English text:': {
+        'capitalised': {
+            'important',
+            'loud',
+            'significant',
+            'public'
+        }
+        
+        'black': {
+            'denotation': {
+                '#0D0D0D'
+            }
+            'connotation': {
+                'serious'
+            }
+        },
+
+        'English text': {
+            'English' : {
+                'Language'
+            }
+            'text': {
+                'typeface': 
+                    'DIN': {
+                        'denotation': {
+
+                        }
+                        'connotation': {
+                            'official',
+                            'serious',
+                            'rigid',
+                            'commanding',
+                            'imperative'
+                        }
+                    }
+                'line': {
+                    'word': {
+                        'letter': 'P',
+                        'letter': 'E',
+                        'letter': 'A',
+                        'letter': 'C',
+                        'letter': 'E'
+                    }
+                },
+                'line': {
+                    'word': {
+                        'letter': 'I',
+                        'letter': 'N'
+                    }
+                    'word': {
+                        'letter': 'T',
+                        'letter': 'H',
+                        'letter': 'E'
+                    }
+                },
+                'line': {
+                    'word': {
+                        'letter':'B',
+                        'letter':'A',
+                        'letter':'L',
+                        'letter':'K',
+                        'letter':'A',
+                        'letter':'N',
+                        'letter':'S'
+                    }
+                }
+            }
+        };
+    }
+    '"PEACE IN THE BALKANS".': {
+        'denotation': {
+            'Sentence': {
+                'Noun Phrase': {
+                    'PEACE': {
+                        'connotation' : {
+                            'ceasefire',
+                            'stop of violence'
+                        }
+                    }
+                }
+                'Preposition Phrase': {
+                    'Preposition': {
+                        'IN'
+                    }
+                    'Noun Phrase': {
+                        'Determiner': {
+                            'THE'
+                        }
+                        'Noun': {
+                            'BALKANS': {
+                                'denotation': {
+                                    'Geographic Area in the south of Europe.'
+                                }
+                                'connotation': {
+                                    'the South',
+                                    'conflicted area'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        },
+        'connotation': {
+            '1990s independence conflicts',
+            'dissolution of Yugoslavia',
+            'civil wars',
+            'UN interventions',
+            'massacre',
+            'genocide'
+            },
+        }
+    }
+}
---
+++ tool_3-drawSteps/python/txt-to-array.py
@@ -0,0 +1,4 @@
+with open('/Users/dmnkvd/KABK/I-IMD/archive-transcoding/Archive-Transcoding-Tools/tool_3-drawSteps/python/steps.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+print(lines)
