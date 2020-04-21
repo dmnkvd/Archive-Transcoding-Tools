5236d6ecbc43e493f1f93dab2915dc9f7c1ffd73
dmnkvd <dominikvr@gmail.com>
2020-04-21 11:55:24 +0200

    arrow function

--- tool_3.1-drawStepsNodeJS/index.js
+++ tool_3.1-drawStepsNodeJS/index.js
@@ -8,15 +8,15 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendfile('index.html');
   });
 
-app.listen(3000, function(){
+app.listen(3000, () => {
   console.log("Listening on port 3000!")
 });
 
-app.post('/receive', function (req, res) {
+app.post('/receive', (req, res) => {
   // The image data will be stored here
   let body = '';
   // Target file path
@@ -29,19 +29,20 @@ app.post('/receive', function (req, res) {
   let filePath = __dirname + `/testWrite/canvas${num}.jpeg`;
 
 // [QUESTION] What are 'data' and 'end' referring to? Are they HTTP events? Is there a list somewhere? Could not find it...
-  req.on('data', function(data) {
+  req.on('data', (data) => {
     body += data;
+    num++
   });
 
   // When whole image has been uploaded.
-  req.on('end', function (){
+  req.on('end', () => {
     // Get rid of the image header as we only need the data parts after it.
     let data = body.replace(/^data:image\/\w+;base64,/, "");
     // Create a buffer and set its encoding to base64
     let buf = new Buffer.from(data, 'base64');
     // Write
     
-    fs.writeFile(filePath, buf, function(err){
+    fs.writeFile(filePath, buf, (err) => {
       if (err) throw err
       // Respond to client that the canvas image is saved.
       res.end();
